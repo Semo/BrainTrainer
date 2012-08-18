09323a7e04a17951f58e3661ee73f3221fb434c3
Semo <semox78@gmail.com>
2012-08-18 22:31:25 +0200

    completed PersonController and Dao with minimum functionality.

--- src/main/java/de/bht/mme2/controller/PersonController.java
+++ src/main/java/de/bht/mme2/controller/PersonController.java
@@ -1,13 +1,17 @@
 package de.bht.mme2.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import de.bht.mme2.dao.PersonDao;
 import de.bht.mme2.domain.Person;
@@ -25,13 +29,25 @@ public class PersonController {
 	@Autowired
 	private PersonService personService;
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/person/add")
+	@RequestMapping(method = RequestMethod.GET, value = "/person/")
+	public ModelAndView getAllPersons() {
+		List<Person> list = personDao.getAllPersons();
+		logger.info("Es sind " + list.size() + " Personen in der Datenbank.");
+		return personService.buildListView(list);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST, value = "/person/add")
 	public String addPerson(@ModelAttribute("person") Person person, BindingResult result){
 		logger.info("Person angelegt: " + person.toString());
 		personDao.addPerson(person);
 		return "redirect:/";
 	}
 	
-	
+	@RequestMapping(method = RequestMethod.DELETE, value = "/person/delete/{id}")
+	public String deletePerson(@PathVariable("id") long id) {
+		logger.info("Lösche Person mit ID: " + id);
+		personDao.deletePerson(id);
+		return "redirect:/";
+	}
 	
 }
--- src/main/java/de/bht/mme2/dao/PersonDao.java
+++ src/main/java/de/bht/mme2/dao/PersonDao.java
@@ -1,12 +1,44 @@
 package de.bht.mme2.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 import de.bht.mme2.domain.Person;
+import de.bht.mme2.service.PersonService;
 
 public class PersonDao {
 
+	@Autowired
+	private PersonService personService;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Transactional
 	public void addPerson(Person person) {
-		// TODO Auto-generated method stub
-		
+		entityManager.merge(person);
+	}
+
+	@Transactional
+	public Boolean deletePerson(long id) {
+		Person deleteMe = personService.findPersonByID(this.getAllPersons(), id);
+		if (deleteMe != null) {
+			entityManager.remove(deleteMe);
+			return true;
+		}
+		return false;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Person> getAllPersons() {
+		Query q = entityManager.createQuery("select * from Person");
+		return q.getResultList();
 	}
 
 }
